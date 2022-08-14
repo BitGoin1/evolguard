fc7b800e9d2db56cb4dde29e9dc7d38136cbb9cd
BitGoin1 <111240627+BitGoin1@users.noreply.github.com>
2022-08-14 21:18:02 +0300

    Add files via upload

---
+++ guard.js
@@ -0,0 +1,372 @@
+const { Client, MessageEmbed } = require("discord.js");
+const client = new Client({ignoreDirect: true, ignoreRoles: true, ignoreEveryone: true});
+const Exeal = require('./Exeal.json')
+client.setMaxListeners(50);
+const request = require("request");
+
+
+const dokunma = Exeal.Dokunma;
+const sunucu = Exeal.sunucuid; 
+
+const ardaban = Exeal.Logs.banlog; 997259853355565237// LOG ID
+const ardakick = Exeal.Logs.kicklog; 997259853355565237// LOG ID
+const ardarol = Exeal.Logs.rollog; 997259853355565237 // LOG ID
+const ardakanal = Exeal.Logs.kanallog; 997259853355565237// LOG ID
+const ardasunucu = Exeal.Logs.sunuculog; 997259853355565237// LOG ID
+const ardawebhook = Exeal.Logs.webhooklog;997259853355565237 // LOG ID
+const ardaurl = Exeal.Logs.urllog; 997259853355565237// LOG ID
+const ardabotkoruma = Exeal.Logs.botkorumalog; 997259853355565237// LOG ID
+const botroles = Exeal.botrolleri; 997259721629245480// Botların Rolleri
+const arr = ["ADMINISTRATOR", "MANAGE_CHANNELS", "MANAGE_GUILD", "KICK_MEMBERS", "BAN_MEMBERS", "MANAGE_ROLES", "MANAGE_WEBHOOKS", "MANAGE_NICKNAMES"]; // Eğer herhangi bir işlemden sonra birşey yapmasını istemiyorsanız içini boş bırakabılırsınız
+
+
+client.on("ready", async () => {
+dokunma.push(client.user.id);
+console.log(`Bot Aktif :)  
+Botun Dokunmayacağı Roller: ${dokunma}`);
+client.user.setStatus("idle");
+client.user.setActivity("BİtGoin", { type: "WATCHING"}); //// TYPE - WATCHING , PLAYING , LISTENING
+});
+
+
+//-----------------------------------BAN KORUMA--------------------------------\\
+
+client.on("guildBanAdd", async (guild, user) => {
+const logs = await guild.fetchAuditLogs({ limit: 1, type: "ADMINISTRATOR" });
+const log = logs.entries.first();
+if (!log) return;
+const target = log.target;
+const id = log.executor.id;
+if (!dokunma.includes(id)) {
+let uye = guild.members.cache.get(id);
+let kullanici = guild.members.cache.get(client.user.id);
+if (kullanici.roles.highest.rawPosition <= uye.roles.highest.rawPosition) return;
+guild.roles.cache.filter(r => {return (arr.some(a => r.permissions.has(a)) && !botroles.includes(r.id) && r.rawPosition < kullanici.roles.highest.rawPosition)}).map(x => {console.log(x.name);
+x.edit({permissions: x.permissions.remove(arr)});});
+uye.ban({reason: "Sunucudan Üye Yasakladığı İçin Yasaklandı.", days: 7});
+guild.members.unban(target.id);
+let yazı = 'Guard | Ban Koruması'
+const ardaembed = new MessageEmbed()
+.setAuthor(yazı, guild.iconURL({dynamic:true}))
+.setDescription(`${uye} (\`${uye.id}\`), **${target.tag}** (\`${target.id}\`) kullanıcısını yasakladı. \n\n ${uye} üyesini sunucudan \`yasakladım\` **${target.tag}** üyesinin banını kaldırdım. `)
+.setColor('#c43636')
+.setFooter(`BitGoin Guard`)
+client.channels.cache.get(ardaban).send(ardaembed)
+} else { };});
+
+//-----------------------------------BAN KORUMA--------------------------------\\
+
+
+
+
+//-----------------------------------KICK KORUMA--------------------------------\\
+
+client.on("guildMemberRemove", async (uye) => {
+let guild = uye.guild;
+const logs = await guild.fetchAuditLogs({ limit: 1, type: "ADMINISTRATOR" });
+const log = logs.entries.first();
+if (!log) return;
+const target = log.target;
+const id = log.executor.id;
+if (!dokunma.includes(id)) {
+if (uye.id === target.id) {
+let user = guild.members.cache.get(id);
+let kullanici = guild.members.cache.get(client.user.id);
+if (kullanici.roles.highest.rawPosition <= uye.roles.highest.rawPosition) return;
+guild.roles.cache.filter(r => {return (arr.some(a => r.permissions.has(a)) && !botroles.includes(r.id) && r.rawPosition < kullanici.roles.highest.rawPosition)}).map(x => {console.log(x.name);
+x.edit({permissions: x.permissions.remove(arr)})});
+user.ban({reason: "Sunucudan Üye Attığı İçin Yasaklandı.", days: 7});
+let yazı = 'Guard | Kick Koruması'
+const arda1embed = new MessageEmbed()
+.setAuthor(yazı, guild.iconURL({dynamic:true}))
+.setDescription(`${user} (\`${user.id}\`), **${target.tag}** (\`${target.id}\`) kullanıcısını attı. \n\n ${user} üyesini sunucudan \`yasakladım.\``)
+.setColor('#c43636')
+.setFooter(`BitGoin Guard`)
+client.channels.cache.get(ardakick).send(arda1embed)
+} else { };
+} else { };});
+
+//-----------------------------------KICK KORUMA--------------------------------\\
+
+
+
+
+
+
+//-----------------------------------WEBHOOK KORUMA--------------------------------\\
+client.on("webhookUpdate", async (channel) => {
+let guild = channel.guild;
+guild.fetchAuditLogs().then(async (logs) => {
+if (logs.entries.first().action === `ADMINISTRATOR`) {
+let yetkili = logs.entries.first().executor;
+let id = logs.entries.first().executor.id;
+if (!dokunma.includes(id)) {
+let uye = guild.members.cache.get(id);
+let kullanic = guild.members.cache.get(client.user.id);
+if (kullanic.roles.highest.rawPosition < uye.roles.highest.rawPosition) return;
+guild.roles.cache.filter(r => {return (arr.some(a => r.permissions.has(a)) && !botroles.includes(r.id) && r.rawPosition < kullanic.roles.highest.rawPosition)}).map(x => {console.log(x.name);
+x.edit({permissions: x.permissions.remove(arr)});});
+uye.ban({reason: "Webhookları Değiştirmekten(açmak-silmek-düzenlemek) yasaklandı.", days: 7});
+let yazı = 'Guard | Webhook Koruması'
+const ardawebhookembed = new MessageEmbed()
+.setAuthor(yazı, guild.iconURL({dynamic:true}))
+.setDescription(`${uye} (\`${uye.id}\`) Webhook Oluşturdu.\n\n ${uye} üyesini sunucudan \`yasakladım.\``)
+.setColor('#c43636')
+.setFooter(`BitGoin Guard`)
+client.channels.cache.get(ardawebhook).send(ardawebhookembed)
+} else { };
+} else { };})});
+//-----------------------------------WEBHOOK KORUMA--------------------------------\\
+
+
+
+
+
+
+
+//-----------------------------------KANAL KORUMA--------------------------------\\
+
+client.on("channelCreate", async (channel) => {
+const guild = channel.guild;
+guild.fetchAuditLogs().then(async (logs) => {
+if(logs.entries.first().action === `ADMINISTRATOR`) {
+const id = logs.entries.first().executor.id;
+if(!dokunma.includes(id)) {
+const uye = guild.members.cache.get(id);
+const kullanici = guild.members.cache.get(client.user.id);
+if(kullanici.roles.highest.rawPosition < uye.roles.highest.rawPosition) return;
+guild.roles.cache.filter(r => {return (arr.some(a => r.permissions.has(a)) && !botroles.includes(r.id) && r.rawPosition < kullanici.roles.highest.rawPosition)}).map(x => {
+console.log(x.name);
+x.edit({permissions: x.permissions.remove(arr)});});
+uye.ban({reason: "Kanal Oluşturmaktan Yasaklandı.", days: 7});
+let yazı = 'Guard | Kanal Koruması'
+const ardakanalembed = new MessageEmbed()
+.setAuthor(yazı, guild.iconURL({dynamic:true}))
+.setDescription(`${uye} (\`${uye.id}\`) Kanal oluşturdu.\n\n ${uye} üyesini sunucudan \`yasakladım.\``)
+.setColor('#c43636')
+.setFooter(`BitGoin Guard`)
+client.channels.cache.get(ardakanal).send(ardakanalembed)
+} else { };} else { };});});
+
+//-----------------------------------KANAL KORUMA--------------------------------\\
+
+
+
+
+
+
+//-----------------------------------KANAL KORUMA--------------------------------\\
+
+client.on("channelDelete", async (channel) => {
+const guild = channel.guild;
+guild.fetchAuditLogs().then(async (logs) => {
+if (logs.entries.first().action === `ADMINISTRATOR`) {
+const id = logs.entries.first().executor.id;
+if (!dokunma.includes(id)) {
+const uye = guild.members.cache.get(id);
+const kullanici = guild.members.cache.get(client.user.id);
+if(kullanici.roles.highest.rawPosition < uye.roles.highest.rawPosition)
+guild.roles.cache.filter(r => {return (arr.some(a => r.permissions.has(a)) && !botroles.includes(r.id) && r.rawPosition < kullanici.roles.highest.rawPosition)}).map(x => {console.log(x.name);
+x.edit({permissions: x.permissions.remove(arr)})});
+uye.ban({reason: "Kanal Silmekten Yasaklandı.", days: 7});
+let yazı = 'Guard | Kanal Koruması'
+const ardaembed3 = new MessageEmbed()
+.setAuthor(yazı, guild.iconURL({dynamic:true}))
+.setDescription(`${uye} (\`${uye.id}\`) ${channel.name} Kanalını Sildi.\n\n ${uye} üyesini sunucudan \`yasakladım.\``)
+.setColor('#c43636')
+.setFooter(`BitGoin Guard`)
+client.channels.cache.get(ardakanal).send(ardaembed3)
+} else { };
+} else { };
+})});
+
+//-----------------------------------KANAL KORUMA--------------------------------\\
+
+
+
+
+//-----------------------------------ROL KORUMA--------------------------------\\
+client.on("roleDelete", async (role) => {
+const guild = role.guild;
+let sil = guild.roles.cache.get(role.id);
+guild.fetchAuditLogs().then(async (logs) => {
+if(logs.entries.first().action === `ADMINISTRATOR`) {
+const id = logs.entries.first().executor.id;
+if(!dokunma.includes(id)) {
+const uye = guild.members.cache.get(id);
+const kullanici = guild.members.cache.get(client.user.id);
+if(kullanici.roles.highest.rawPosition < uye.roles.highest.rawPosition)
+guild.roles.cache.filter(r => {return (arr.some(a => r.permissions.has(a)) && !botroles.includes(r.id) && r.rawPosition < kullanici.roles.highest.rawPosition)}).map(x => {console.log(x.name); x.edit({permissions: x.permissions.remove(arr)});});
+uye.ban({reason: "Rol Silmekten Yasaklandı.", days: 7});
+let yazı = 'Guard | Rol Koruması'  
+const ardaembed2 = new MessageEmbed()
+.setAuthor(yazı, guild.iconURL({dynamic:true}))
+.setDescription(`${uye} (\`${uye.id}\`) ${role.name} Rolünü Sildi.\n\n ${uye} üyesini sunucudan \`yasakladım.\``)
+.setColor('#acaa37')
+.setFooter(`BitGoin Guard`)
+client.channels.cache.get(ardarol).send(ardaembed2)
+} else { };
+} else { };});});
+//-----------------------------------ROL KORUMA--------------------------------\\
+
+
+
+
+
+//-----------------------------------ROL KORUMA--------------------------------\\
+client.on("roleUpdate", async (oldRole, newRole) => { 
+let guild = newRole.guild;
+guild.fetchAuditLogs().then(async (logs) => {
+if(logs.entries.first().action === `ADMINISTRATOR`) {
+let id = logs.entries.first().executor.id;
+if(!dokunma.includes(id)) {
+if(!arr.some(a => oldRole.permissions.has(a)) && arr.some(a => newRole.permissions.has(a))) {
+const uye = guild.members.cache.get(id);
+const kullanici = guild.members.cache.get(client.user.id);
+if(kullanici.roles.highest.rawPosition < uye.roles.highest.rawPosition) return;
+guild.roles.cache.filter(r => { return ( arr.some(a => r.permissions.has(a)) && !botroles.includes(r.id) && r.rawPosition < kullanici.roles.highest.rawPosition)}).map(x => {
+console.log(x.name);
+x.edit({permissions: x.permissions.remove(arr)});
+});
+uye.ban({reason: "Rol Güncellemekten Yasaklandı.", days: 7});
+let yazı = 'Guard | Rol Koruması'  
+const arda1919embed = new MessageEmbed()
+.setAuthor(yazı, guild.iconURL({dynamic:true}))
+.setDescription(`${uye} (\`${uye.id}\`) ${newRole.name} Rolüne Yetki Verdi.\n\n ${uye} üyesini sunucudan \`yasakladım.\``)
+.setColor('#378fac')
+.setFooter(`BitGoin Guard`)
+client.channels.cache.get(ardarol).send(arda1919embed)
+} else { };
+} else { };
+} else { };
+});
+});
+//-----------------------------------ROL KORUMA--------------------------------\\
+
+
+
+
+
+//-----------------------------------ROL KORUMA--------------------------------\\
+client.on("roleCreate", async (role) => {
+let guild = role.guild;
+guild.fetchAuditLogs().then(async (logs) => {
+if(logs.entries.first().action === `ADMINISTRATOR`) {
+let id = logs.entries.first().executor.id;
+if(!dokunma.includes(id)) {
+let uye = guild.members.cache.get(id);
+let kullanici = guild.members.cache.get(client.user.id);
+if(kullanici.roles.highest.rawPosition < uye.roles.highest.rawPosition) return;
+guild.roles.cache.filter(r => {return (arr.some(a => r.permissions.has(a)) && !botroles.includes(r.id) && r.rawPosition < kullanici.roles.highest.rawPosition)}).map(q => {
+console.log(q.name);
+q.edit({permissions: q.permissions.remove(arr)});});
+uye.ban({reason: "Rol Oluşturmaktan Yasaklandı"});
+role.delete();
+let yazı = 'Guard | Rol Koruması'  
+const ardaembedrolaçmakoruma = new MessageEmbed()
+.setAuthor(yazı, guild.iconURL({dynamic:true}))
+.setDescription(`${uye} (\`${uye.id}\`) Rol Oluşturuldu.\n\n ${uye} üyesini sunucudan \`yasakladım.\``)
+.setColor('#37ac6c')
+.setFooter(`BitGoin Guard`)
+client.channels.cache.get(ardarol).send(ardaembedrolaçmakoruma)
+} else { };
+} else { };});});
+//-----------------------------------ROL KORUMA--------------------------------\\
+
+
+
+
+
+
+//-----------------------------------BOT KORUMA--------------------------------\\
+client.on("guildMemberAdd", async (member) => {
+const guild = member.guild;
+guild.fetchAuditLogs().then(async (logs) => {
+if(logs.entries.first().action === `ADMINISTRATOR`) {
+const id = logs.entries.first().executor.id;
+if(!dokunma.includes(id)) {
+if(member.user.bot){
+const uye = guild.members.cache.get(id);
+const kullanici = guild.members.cache.get(client.user.id);
+if(kullanici.roles.highest.rawPosition < uye.roles.highest.rawPosition) return;
+guild.roles.cache.filter(r => { return (arr.some(a => r.permissions.has(a)) && !botroles.includes(r.id) && r.rawPosition < kullanici.roles.highest.rawPosition)}).map(x => { console.log(x.name);
+x.edit({permissions: x.permissions.remove(arr)});});
+uye.ban({ reason: "Sunucuya Bot Getirdiği İçin Yasaklandı.", days: 7 });
+member.ban({ reason: "Sunucuya Bot İzinsiz Çekildi.", days: 7 })
+let yazı = 'Guard | Bot Koruması'  
+const ardabotembed = new MessageEmbed()
+.setAuthor(yazı, guild.iconURL({dynamic:true}))
+.setDescription(`${uye} (\`${uye.id}\`) Sunucuya ${member} Botunu Ekledi.\n\n ${uye} üyesini sunucudan \`yasakladım.\``)
+.setColor('#5ba4cc')
+.setFooter(`BitGoin Guard`)
+client.channels.cache.get(ardabotkoruma).send(ardabotembed)
+} else { };
+} else { };
+} else { };});});
+//-----------------------------------BOT KORUMA--------------------------------\\
+
+
+
+
+
+//-----------------------------------SUNUCU KORUMA--------------------------------\\
+
+client.on("guildUpdate", async (oldGuild, newGuild) => {
+newGuild.fetchAuditLogs().then(async (logs) => {
+if(logs.entries.first().action === `ADMINISTRATOR`) {
+var yapan = logs.entries.first().executor;
+let id = yapan.id;
+const uye = newGuild.members.cache.get(id);
+const kullanici = newGuild.members.cache.get(client.user.id);
+if(oldGuild.vanityURLCode !== newGuild.vanityURLCode) {
+if(!dokunma.includes(id)) {
+request({
+method: "PATCH",
+url: `https://discord.com/api/guilds/${newGuild.id}/vanity-url`,
+headers: {
+Authorization: `Bot ${client.token}`},
+json: {code: `${oldGuild.vanityURLCode}`}});
+newGuild.roles.cache.filter(r => { return(arr.some(a => r.permissions.has(a)) && !botroles.includes(r.id) && r.rawPosition < kullanici.roles.highest.rawPosition);}).map(x => {console.log(x.name);
+x.edit({permissions: x.permissions.remove(arr)});});
+uye.ban({reason: "Url'yi Değiştirdiği İçin Yasaklandı.", days: 7});
+let yazı = 'Guard | Url Koruması'  
+const ardaurlembed = new MessageEmbed()
+.setAuthor(yazı, newGuild.iconURL({dynamic:true}))
+.setDescription(`${uye} (\`${uye.id}\`) Sunucunun Urlsini Değiştirdi.\n\n Sunucunun Urlsini \`${newGuild.vanityURLCode}\` Olarak Değiştirdi, ${uye} üyesini sunucudan \`yasakladım.\``)
+.setColor('#c43636')
+.setFooter(`BitGoin Guard`)
+client.channels.cache.get(ardaurl).send(ardaurlembed)
+} else { };
+} else if (oldGuild.name !== newGuild.name) {
+if(!dokunma.includes(id)) {
+newGuild.setName(oldGuild.name);
+uye.ban({reason: "Sunucunun İsmini Değiştirdiği İçin Yasaklandı", days: 7});
+newGuild.roles.cache.filter(r => { return (arr.some(a => r.permissions.has(a)) && !botroles.includes(r.id) && r.rawPosition < kullanici.roles.highest.rawPosition)}).map(x => { console.log(x.name);
+x.edit({permissions: x.permissions.remove(arr)});
+let yazı = 'Guard | Sunucu Koruması'  
+const ardasunucuembed = new MessageEmbed()
+.setAuthor(yazı, newGuild.iconURL({dynamic:true}))
+.setDescription(`${uye} (\`${uye.id}\`) Sunucu Ayarlarını Güncelledi.\n\n ${uye} üyesini sunucudan \`yasakladım.\``)
+.setColor('#c43636')
+.setFooter(`BitGoin Guard`)
+client.channels.cache.get(ardasunucu).send(ardasunucuembed)});
+} else { };
+} else { };
+} else { };});});
+
+process.on("uncaughtExpection", function (err) {
+  if (err) console.log(err);
+})
+  client.on("ready",() => {
+    client.channels.cache.get('997259802801614909').join();
+});
+
+//-----------------------------------SUNUCU KORUMA--------------------------------\\
+
+client.login('OTc4NjUyNzkxMjcyNzcxNTk0.GkW2fQ.ZmyDQ3Ppw9RJuBf1lOE39LoY_BoeOPXV51gCjQ'); // Glitchde kullanacaklar varsa process.env.token yazıp env e token girebilirler
+
+
+
+
+
+
